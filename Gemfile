4e9849126d783e16439f598e96d385d386d9ba8e
yuu imai <imaiman2009@yahoo.co.jp>
2020-02-11 21:15:36 +0900

    ログイン機能の作成

--- Gemfile
+++ Gemfile
@@ -33,3 +33,4 @@ group :test do
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'bcrypt', '3.1.11'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   resources :blogs do
     collection do
       post :confirm
