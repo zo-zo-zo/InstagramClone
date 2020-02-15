b58f2524f75bde9b2bddf8fcb4a90601decc5724
yuu imai <imaiman2009@yahoo.co.jp>
2020-02-15 20:52:50 +0900

    userのプロフィール編集画面の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -12,12 +14,26 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice:"プロフィールを編集しました"
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
