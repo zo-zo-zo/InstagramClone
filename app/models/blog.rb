c18fa2ed0235867918a144abdb065a2774282d63
yuu imai <imaiman2009@yahoo.co.jp>
2020-02-21 03:09:16 +0900

    CSSの調整

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,7 @@
 class Blog < ApplicationRecord
   validates :title, :content, presence: true
   belongs_to :user
+  THUMBNAIL_SIZE = [100, 100]
   mount_uploader :picture, PictureUploader
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :favorites, only: [:create, :destroy, :show]
