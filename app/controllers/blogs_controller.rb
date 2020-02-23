b945a7d60557d5900d3c83359b6d1e060d7b997d
yuu imai <imaiman2009@yahoo.co.jp>
2020-02-23 17:54:42 +0900

    レターオープナーに関する項目をコメントアウト

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -21,7 +21,7 @@ class BlogsController < ApplicationController
       render :new
     else
       if @blog.save
-        CheckMailer.check_mail(@blog).deliver
+        # CheckMailer.check_mail(@blog).deliver
         redirect_to blogs_path, notice: "ブログを作成しました！"
       else
         render :new
